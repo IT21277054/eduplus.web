9bcf75e0dfcb4ddafb5c2e4f5c41f95da40f79ae
Shamry Shiraz <IT21277054@my.sliit.lk>
2024-05-15 13:46:12 +0530

    Update Enroll.tsx

--- Edu+/src/components/enrollmentManagment/Enroll.tsx
+++ Edu+/src/components/enrollmentManagment/Enroll.tsx
@@ -12,6 +12,36 @@ import {
 import { Footer } from '../templates/Footer';
 import Header from '../templates/Header';
 
+const descriptions = [
+  'Introduction to SPM',
+  'SPM Part Two',
+  'SPM Part Three',
+  'Assignment',
+  'Part Four',
+  'Part Five',
+  'Part Six',
+  'Part Seven',
+];
+
+const quizDescriptions = [
+  'Quiz One',
+  'Quiz Two',
+  'Quiz Three',
+  'Quiz Four',
+  'Quiz Five',
+  'Quiz Six',
+  'Quiz Seven',
+  'Quiz Eight',
+];
+
+const imageUrls = [
+  'https://img.freepik.com/free-vector/learning-concept-illustration_114360-6186.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5dKOEC7_1Frv1if6SxAh8SZ5Dcr0N4T6TgvJZTWu0svjRsOHqn7DvVMlSE9VJxvwMP7o&usqp=CAU',
+  'https://img.freepik.com/free-vector/flat-university-concept_23-2148170517.jpg',
+  'https://img.freepik.com/free-vector/back-school-supplies-flat-style_24908-56845.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnNsJ1vRuBlR1OK8JuAKLA_KJvKrYUMEn5jX8uKGQZLF_x047xhMZUk48FexurqcFgel0&usqp=CAU',
+];
+
 function Entoll() {
   return (
     <>
@@ -25,7 +55,7 @@ function Entoll() {
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center',
-          backgroundImage: `url(https://t4.ftcdn.net/jpg/05/25/08/09/360_F_525080936_JEpnKXh2siYKBPpsqd98pbbcIzy4ySKz.jpg)`,
+          backgroundImage: `url(https://forte.com.tr/assets/imgs/blog/80.webp)`,
         }}
       >
         {/* Increase the priority of the hero background image */}
@@ -60,10 +90,10 @@ function Entoll() {
                 color="inherit"
                 gutterBottom
               >
-                Course Name
+                SPM
               </Typography>
               <Typography variant="h5" color="inherit" paragraph>
-                Sub Title
+                Software Process Management
               </Typography>
             </Box>
           </Grid>
@@ -88,13 +118,14 @@ function Entoll() {
               <Card sx={{ display: 'flex', minWidth: 345, marginRight: 2 }}>
                 <CardContent sx={{ flex: 1 }}>
                   <Typography component="h2" variant="h5">
-                    Lecture
+                    Lecture {index + 1}
                   </Typography>
                   <Typography variant="subtitle1" color="text.secondary">
-                    Sub title
+                    SPM2024
                   </Typography>
                   <Typography variant="subtitle1" paragraph>
-                    123
+                    {/* Replace this line with your array of descriptions */}
+                    {descriptions[index % descriptions.length]}
                   </Typography>
                   <Typography variant="subtitle1" color="primary">
                     Watch Lecture
@@ -103,7 +134,8 @@ function Entoll() {
                 <CardMedia
                   component="img"
                   sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-                  image="https://t4.ftcdn.net/jpg/05/25/08/09/360_F_525080936_JEpnKXh2siYKBPpsqd98pbbcIzy4ySKz.jpg"
+                  image={imageUrls[index % imageUrls.length]}
+                  alt={`Lecture ${index + 1}`}
                 />
               </Card>
             </CardActionArea>
@@ -129,13 +161,14 @@ function Entoll() {
               <Card sx={{ display: 'flex', minWidth: 345, marginRight: 2 }}>
                 <CardContent sx={{ flex: 1 }}>
                   <Typography component="h2" variant="h5">
-                    Quiz
+                    Quiz {index + 1}
                   </Typography>
                   <Typography variant="subtitle1" color="text.secondary">
-                    Sub title
+                    SPM2024
                   </Typography>
                   <Typography variant="subtitle1" paragraph>
-                    123
+                    {/* Replace this line with your array of descriptions */}
+                    {quizDescriptions[index % quizDescriptions.length]}
                   </Typography>
                   <Typography variant="subtitle1" color="primary">
                     Do Quiz
@@ -144,7 +177,7 @@ function Entoll() {
                 <CardMedia
                   component="img"
                   sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-                  image="https://t4.ftcdn.net/jpg/05/25/08/09/360_F_525080936_JEpnKXh2siYKBPpsqd98pbbcIzy4ySKz.jpg"
+                  image="https://www.quiz-maker.com/images/make-a-quiz.png"
                 />
               </Card>
             </CardActionArea>
